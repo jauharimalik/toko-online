88a33a97570d731f0d1de960c0351b859d667ed2
jauharimalik <jauharimalikupil@gmail.com>
2023-09-10 11:16:03 +0700

    sql

--- elesindoshop.sql
+++ elesindoshop.sql
@@ -16,8 +16,6 @@
 
 
 -- Dumping database structure for elesindoshop
-CREATE DATABASE IF NOT EXISTS `elesindoshop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `elesindoshop`;
 
 -- Dumping structure for table elesindoshop.addons
 CREATE TABLE IF NOT EXISTS `addons` (
